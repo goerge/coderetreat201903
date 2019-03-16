a066d65e2653d8533a2aeaf393c6e914c9dd8f26
Görge Albrecht <goerge@code-mentor.com>
2019-03-16 06:43:06 +0100

    first failing test

---
+++ src/test/java/com/codementor/coderetreat201903/FirstTest.java
@@ -0,0 +1,13 @@
+package com.codementor.coderetreat201903;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class FirstTest {
+
+    @Test
+    void name() {
+        fail();
+    }
+}
