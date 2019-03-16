37eff08c6b767cf68de433782e739982673e2a0b
Görge Albrecht <goerge@code-mentor.com>
2019-03-16 13:42:12 +0100

    TCR dummy test green

--- src/test/java/com/codementor/coderetreat201903/FirstTest.java
+++ src/test/java/com/codementor/coderetreat201903/FirstTest.java
@@ -11,7 +11,7 @@ class FirstTest {
 
     @Test
     void failing() {
-        var coderetreat = "";
+        var coderetreat = "x";
         assertThat(coderetreat, is(not(emptyString())));
     }
 }
