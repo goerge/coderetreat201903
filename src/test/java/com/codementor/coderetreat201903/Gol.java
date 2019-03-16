3dd485f34ebed2e98462ec2f4b43a984a73f3f0f
Görge Albrecht <goerge@code-mentor.com>
2019-03-16 14:44:14 +0100

    replace if with ?:

--- src/test/java/com/codementor/coderetreat201903/Gol.java
+++ src/test/java/com/codementor/coderetreat201903/Gol.java
@@ -6,8 +6,6 @@ import static com.codementor.coderetreat201903.Cell.LIVING;
 public class Gol {
 
   public Cell tick(Cell cell, int neighbours) {
-    if(neighbours == 2)
-      return cell;
-    return neighbours == 3 ? LIVING : DEAD;
+    return neighbours == 2 ? cell : neighbours == 3 ? LIVING : DEAD;
   }
 }
