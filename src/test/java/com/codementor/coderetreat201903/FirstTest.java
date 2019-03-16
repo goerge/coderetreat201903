07e42503ede6c4e5a2e01b25dd320ad416376669
Görge Albrecht <goerge@code-mentor.com>
2019-03-16 06:49:09 +0100

    adding Hamcrest

--- src/test/java/com/codementor/coderetreat201903/FirstTest.java
+++ src/test/java/com/codementor/coderetreat201903/FirstTest.java
@@ -2,12 +2,16 @@ package com.codementor.coderetreat201903;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 class FirstTest {
 
     @Test
-    void name() {
-        fail();
+    void failing() {
+        var coderetreat = "";
+        assertThat(coderetreat, is(not(emptyString())));
     }
 }
