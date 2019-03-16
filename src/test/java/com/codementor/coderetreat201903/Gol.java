e8e4b518d8fc1909b2c60bcfbefd522871a781ab
Görge Albrecht <goerge@code-mentor.com>
2019-03-16 14:48:08 +0100

    convert to switch

--- src/test/java/com/codementor/coderetreat201903/Gol.java
+++ src/test/java/com/codementor/coderetreat201903/Gol.java
@@ -6,6 +6,13 @@ import static com.codementor.coderetreat201903.Cell.LIVING;
 public class Gol {
 
   public Cell tick(Cell cell, int neighbours) {
-    return neighbours == 2 ? cell : neighbours == 3 ? LIVING : DEAD;
+    switch (neighbours) {
+      case 2:
+        return cell;
+      case 3:
+        return LIVING;
+      default:
+        return DEAD;
+    }
   }
 }
